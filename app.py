268404d8e837df3a7e8472e16e34790988f453d2
yas1k <student@FB.LOCAL>
2024-09-11 16:44:23 +0700

    информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!doctype html>\
         <html>\
             <body>\
                 <h1>web-сервер на flask</h1>\
+                <a href="/author">author</a>\
             </body>\
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Янсон Андрей Алексеевич"
+    group = "ФБИ-21"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
         </html>"""
