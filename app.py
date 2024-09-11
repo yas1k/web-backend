55540549335723de7791cf2814dd97d0b38d219b
yas1k <student@FB.LOCAL>
2024-09-11 17:58:01 +0700

    очистка счетчика

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ def web():
             <body>\
                 <h1>web-сервер на flask</h1>\
                 <a href="/author">author</a>\
+                <a href="/lab1/oak">oak</a>\
+                <a href="/lab1/counter">counter</a>\
+                <a href="/lab1/created">created</a>\
             </body>\
         </html>""", 200, {
             'X-Server': 'sample',
@@ -33,12 +36,16 @@ def author():
 @app.route('/lab1/oak')
 def oak():
     path = url_for("static", filename="oak.jpg")
+    css= url_for("static", filename ="lab1.css")
     return '''
     <!doctype html>
     <html>
+        <link rel="stylesheet" href="static/lab1.css">
         <body>
+            
             <h1>Дуб</h1>
             <img src="''' + path +  '''">
+            <a href="/web">web</a>
         </body>
     </html>
     '''
@@ -54,6 +61,8 @@ def counter():
     <html>
         <body>
             Сколько раз мы сюда заходили: ''' + str(count) + '''
+            <a href="/web">web</a>
+            <a href="/lab1/erase">erase</a>
         </body>
     </html>
     '''
@@ -70,6 +79,7 @@ def created():
         <body>
             <h1> Создано успешно</h1>
             <div><i>что-то создано...</i></div>
+            <a href="/web">web</a>
         </body>
     </html>
     ''', 201
@@ -77,3 +87,16 @@ def created():
 @app.errorhandler(404)
 def not_found(err):
     return "нет такой страницы", 404
+
+@app.route("/lab1/erase")
+def erase():
+    global count
+    count = 0
+    return'''
+    <!doctype html>
+    <html>
+        <body>
+            <h1>Счетчик обнулен</h1>
+            <a href="/lab1/counter">counter</a>
+        </body>
+    </html>'''
