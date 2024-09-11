8c6dac50ba9cf1512e6dec8abe5005e9dc871a9f
yas1k <student@FB.LOCAL>
2024-09-11 16:52:06 +0700

    дуб

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/web")
@@ -26,3 +26,16 @@ def author():
                 <a href="/web">web</a>
             </body>
         </html>"""
+
+@app.route('/lab1/oak')
+def oak():
+    path = url_for("static", filename="oak.jpg")
+    return '''
+    <!doctype html>
+    <html>
+        <body>
+            <h1>Дуб</h1>
+            <img src="''' + path +  '''">
+        </body>
+    </html>
+    '''
