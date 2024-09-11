5d0becc4ffd255cee154bec89382f8df5ae60f21
yas1k <student@FB.LOCAL>
2024-09-11 16:33:09 +0700

    вывод страницы первой лабы

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return "<!doctype html>"\
+        "<html>"\
+            "<body>"\
+                "<h1>web-сервер на flask</h1>"\
+            "</body>"\
+        "</html>"
