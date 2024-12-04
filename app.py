598b03823939c90e47356f0be365781c5125dfb1
yas1k <yanson-andrei@mail.ru>
2024-12-04 10:12:07 +0300

    имя студента

--- app.py
+++ app.py
@@ -342,4 +342,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Янсон Андрей'
+    return render_template('example.html', name=name)
