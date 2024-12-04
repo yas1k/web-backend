f869c6830c610260841d1d007c64b1a16068aa23
yas1k <yanson-andrei@mail.ru>
2024-12-04 10:10:12 +0300

    заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template
 app = Flask(__name__)
 
 @app.route("/index")
@@ -339,3 +339,7 @@ def add_flower(name):
     </body>
 </html>
 '''
+
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
