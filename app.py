696e8ee9fb96991911a866a278fa999554aae2d4
yas1k <yanson-andrei@mail.ru>
2024-12-04 11:19:29 +0300

    филтры

--- app.py
+++ app.py
@@ -358,3 +358,8 @@ def example():
 @app.route('/lab2/')
 def lab2():
     return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filters():
+    phrase = "О сколько нам открытий чудных..."
+    return render_template('filter.html', phrase=phrase)
