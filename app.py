1c26995aeedc267826b38cd3e840d1359af90314
yas1k <yanson-andrei@mail.ru>
2024-12-04 09:55:20 +0300

    вывод цветка

--- app.py
+++ app.py
@@ -315,3 +315,12 @@ def a():
 @app.route('/lab2/a/')
 def a2():
     return 'со слэшем'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        return "такого цветка нет", 404
+    else:
+        return "цветок: " + flower_list[flower_id]
