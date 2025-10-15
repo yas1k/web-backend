fde4c0d8939d00b83aecb0e3df25b85b5b7c1bbc
Янсон Андрей <fbi-31@FB.LOCAL>
2025-10-15 17:59:46 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -353,4 +353,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Янсон Андрей'
-    return render_template('example.html', name=name)
+    lab_num = '2'
+    group = 'ФБИ-31'
+    course_num = '3'
+    return render_template('example.html', name=name, lab_num=lab_num, group=group, course_num=course_num)
