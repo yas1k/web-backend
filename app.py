b02bf0673bcfeed17140c926d6209255d986d5e1
Янсон Андрей <fbi-31@fb.local>
2025-10-15 17:46:15 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ def a():
 def a2():
     return 'со слешэм'
 
-flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -334,3 +334,18 @@ def flowers(flower_id):
         abort(404)
     else:
         return "цветок: " + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1> Добавлен новый цветок </h1>
+    <p> Название нового цветка: {name} </p>
+    <p> Всего цветов: {len(flower_list)} </p>
+    <p> Полный список: {flower_list} </p>
+    </body>
+</html>
+'''
