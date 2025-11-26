f4dcd02ddf2071f28c55e5258bd1742ff3727e6f
yas1k <yanson-andrei@mail.ru>
2025-11-26 07:47:48 +0700

    Книги

---
+++ Desktop/3 курс/я делаю/web-backend-main/app.py
@@ -0,0 +1,454 @@
+from flask import Flask, url_for, redirect, request, abort, render_template
+import datetime
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+    return '''
+    <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <head>
+            <title> НГТУ, ФБ, Лабораторные работы </title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+                <hr>
+            </header>
+            <main>
+                <a href="/lab1">Первая лабораторная работа</a>
+            </main>
+            <footer>
+                <hr>
+                &copy; Янсон Андрей, ФБИ-21, 3 курс, 2024 год
+            </footer>
+        </body>    
+    </html>'''
+
+@app.route("/")
+def index2():
+    return '''
+    <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <head>
+            <title> НГТУ, ФБ, Лабораторные работы </title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+                <hr>
+            </header>
+            <main>
+                <a href="/lab1">Первая лабораторная работа</a>
+            </main>
+            <footer>
+                <hr>
+                &copy; Янсон Андрей, ФБИ-21, 3 курс, 2024 год
+            </footer>
+        </body>    
+    </html>'''
+
+@app.route("/lab1")
+def lab1():
+    return '''
+    <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <head>
+            <title> Лабораторная работа </title>
+        </head>
+        <body>
+            <main>
+                <p> Flask - фреймворк для создания веб-приложений на языке программирования
+                Python, использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2.
+                Относится к категории так называемых микрофреймворков - минималистичных каркасов
+                веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+                </p>
+                <a href ="/">Корень сайта</a>
+                <h2>Список роутов</h2>
+                <a href ="/lab1/web">Сайт "web"</a>
+                <a href ="/lab1/author">Сайт "author"</a>
+                <a href ="/lab1/image">Сайт "oak"</a>
+                <a href ="/lab1/counter">Сайт "counter"</a>
+                <a href ="/lab1/info">Сайт "info"</a>
+                <a href ="/lab1/created">Сайт "created"</a>
+                <a href ="/lab1/erase">Сайт "erase"</a>
+                <a href ="/lab1/400">Сайт "400"</a>
+                <a href ="/lab1/401">Сайт "401"</a>
+                <a href ="/lab1/402">Сайт "402"</a>
+                <a href ="/lab1/403">Сайт "403"</a>
+                <a href ="/lab1/405">Сайт "405"</a>
+                <a href ="/lab1/418">Сайт "418"</a>
+                <a href ="/lab1/error500">Сайт "error500"</a>
+                <a href ="/lab1/custom">Сайт "custom"</a>
+
+            </main>
+            <footer>
+                <hr>
+                &copy; Янсон Андрей, ФБИ-21, 3 курс, 2024 год
+            </footer>
+        </body>    
+    </html>
+    '''
+
+@app.route("/lab1/web")
+def web():
+    return """<!doctype html>\
+        <html>\
+        <link rel="stylesheet" href="/static/lab1.css">\
+            <body>\
+                <h1>web-сервер на flask</h1>\
+                <a href="/lab1/author">author</a>\
+                <a href="/lab1/oak">oak</a>\
+                <a href="/lab1/counter">counter</a>\
+                <a href="/lab1/created">created</a>\
+            </body>\
+        </html>""", 200, {
+            'X-Server': 'sample',
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
+
+@app.route("/lab1/author")
+def author():
+    name = "Янсон Андрей Алексеевич"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+        <link rel="stylesheet" href="/static/lab1.css">
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/lab1/web">web</a>
+            </body>
+        </html>"""
+
+@app.route('/lab1/image')
+def oak():
+    path = url_for("static", filename="oak.jpg")
+    css= url_for("static", filename ="lab1.css")
+    return '''
+    <!doctype html>
+    <html>
+        <link rel="stylesheet" href="/static/lab1.css">
+        <body>
+            
+            <h1>Дуб</h1>
+            <img src="''' + path +  '''">
+            <a href="/lab1/web">web</a>
+        </body>
+    </html>
+    ''', {
+            'Content-language': 'ru'
+        }
+
+count = 0
+
+@app.route('/lab1/counter')
+def counter():
+    global count
+    count += 1
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+    return '''
+    <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <body>
+            Сколько раз мы сюда заходили: ''' + str(count) + '''
+            <a href="/lab1/web">web</a>
+            <a href="/lab1/erase">erase</a>
+            <hr>
+            Дата и время: ''' + str(time) + '''<br>
+            Запрошенный адрес: ''' + url + '''<br>
+            Ваш IP адресс: ''' + client_ip + '''<br>
+        </body>
+    </html>
+    '''
+
+@app.route("/lab1/info")
+def info():
+    return redirect("/lab1/author")
+
+@app.route("/lab1/created")
+def created():
+    return'''
+    <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <body>
+            <h1> Создано успешно</h1>
+            <div><i>что-то создано...</i></div>
+            <a href="/lab1/web">web</a>
+        </body>
+    </html>
+    ''', 201
+
+@app.errorhandler(404)
+def not_found(err):
+    path = url_for("static", filename="mistake.jpg")
+    return'''
+         <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <head>
+            <title> Лабораторная работа </title>
+        </head>
+        <body>
+            <main>
+                <h1> Такой страницы не существует!
+                Вы явно что-то перепутали...
+                </h1>
+                <img src="''' + path +  '''">
+            </main>
+            <footer>
+                <hr>
+                &copy; Янсон Андрей, ФБИ-21, 3 курс, 2024 год
+            </footer>
+        </body>    
+    </html>
+     ''', 404
+
+@app.route("/lab1/erase")
+def erase():
+    global count
+    count = 0
+    return'''
+    <!doctype html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <body>
+            <h1>Счетчик обнулен</h1>
+            <a href="/lab1/counter">counter</a>
+        </body>
+    </html>'''
+
+@app.route("/lab1/400")
+def fh():
+    return'''
+    сервер не понял, что от него хотят.
+    ''', 400
+
+@app.route("/lab1/401")
+def fho():
+    return'''
+    Ошибка 401 выдается сервером в случае возникновения проблем
+     с аутентификацией или авторизацией на сайте.
+    ''', 401
+
+@app.route("/lab1/402")
+def fht():
+    return'''
+    это нестандартная ошибка клиента, зарезервированная для использования в будущем.
+    ''', 402
+
+@app.route("/lab1/403")
+def fhth():
+    return'''
+    Ошибка 403 (Forbidden) — это когда сервер понял запрос,
+     но почему-то отказывается выполнять его и отдавать браузеру HTML-код страницы.
+    ''', 403
+
+@app.route("/lab1/405")
+def fhf():
+    return'''
+    Ошибки HTTP 405 возникают, когда метод HTTP не разрешен веб-сервером для запрошенного URL-адреса.
+    ''', 405
+
+@app.route("/lab1/418")
+def fhei():
+    return'''
+    HTTP код ошибки 418 I'm a teapot сообщает о том,
+    что сервер не может приготовить кофе, потому что он чайник.
+    ''', 418
+
+@app.route('/lab1/error500')
+def trigger_error():
+    return 1 / 0
+
+@app.errorhandler(500)
+def internal_error(err):
+    path = url_for("static", filename="laugh.jpg")
+    return '''
+    <!DOCTYPE html>
+    <html>
+    <link rel="stylesheet" href="/static/lab1.css">
+        <head>
+            <title>Ошибка на сервере</title>
+        </head>
+        <body>
+            <div>
+                <h1>Произошла ошибка на сервере</h1>
+                <img src="''' + path +  '''">
+                <p>К сожалению, при обработке вашего запроса возникла ошибка. Не вините себя, может когда-нибудь всё наладится...</p>
+            </div>
+        </body>
+    </html>
+    ''', 500
+
+@app.route('/lab1/custom')
+def custom_route():
+    path_to_img = url_for('static', filename='oak.jpg')
+    return '''
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="/static/lab1.css">
+<head>
+    <title>НГТУ, ФБ, Лабораторные работы</title>
+</head>
+<body>
+    <div>
+        <h1>7 причин почему рыбы хорошие друзья!</h1>
+        <img src=''' + path_to_img + '''>
+        <p>1.Рыбы оказывают на вас очень успокаивающее действие!</p>
+        <p>2.Рыбы многофункциональные рассказчики.</p>
+        <p>3.Рыбы не позволят вам доверять всему и всем</p>
+        <h2>Я думал я перечисляю почему рыбки хорошие друзья, а оказалось в статье писали про знак зодиака... Поэтому я не буду перечислять дальше. Не верю в гороскопы
+    </div>
+</body>
+</html>
+    ''', 200, {
+        'Content-Language': 'ru',
+        'X-Custom-Header-1': '',
+        'X-Custom-Header-2': ''
+    }
+
+@app.route('/lab2/a')
+def a():
+    return 'без слеша'
+
+@app.route('/lab2/a/')
+def a2():
+    return 'со слешэм'
+
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        abort(404)
+    else:
+        return f'''
+    <!doctype html>
+    <html>
+        <body>
+        <h1> Вы успешно выбрали конкретный цветок из списка </h1>
+        <p>Выбранный вами цветок: {flower_list[flower_id]}
+        <a href ="/lab2/all_flowers">Все цветы</a>
+        </body>
+    </html>
+    
+    '''
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1> Добавлен новый цветок </h1>
+    <p> Название нового цветка: {name} </p>
+    <p> Всего цветов: {len(flower_list)} </p>
+    <p> Полный список: {flower_list} </p>
+    </body>
+</html>
+'''
+
+@app.route('/lab2/example')
+def example():
+    name, lab_num,  group,  course_num  ='Янсон Андрей', '2', 'ФБИ-31',  '3'
+    fruits = [
+        {'name': 'Яблоки', 'price': 100},
+        {'name': 'Груши', 'price': 120},
+        {'name': 'Апельсины', 'price': 80},
+        {'name': 'Мандарины', 'price': 95},
+        {'name': 'Манго', 'price': 321}
+        ]
+    return render_template('example.html', name=name, lab_num=lab_num, group=group, course_num=course_num, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filters():
+    phrase = "О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
+    return render_template('filter.html', phrase = phrase)
+
+@app.route('/lab2/add_flower/')
+def noname():
+    return'''
+    УЖАС! ВЫ ЗАБЫЛИ ЗАДАТЬ ИМЯ ЦВЕТКА! НА ЧТО ВЫ РАССЧИТЫВАЛИ?
+    ''', 400
+
+@app.route('/lab2/all_flowers')
+def all_flowers():
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <p> Количество цветов: {len(flower_list)} </p>
+    <p> Полный список всех цветов: {flower_list} </p>
+    </body>
+</html>
+'''
+@app.route('/lab2/no_flowers')
+def no_flowers():
+    global flower_list
+    flower_list = []
+    return f'''
+    <p> Вы очистили список цветов </p>
+    <a href ="/lab2/all_flowers">Все цветы</a>
+'''
+
+@app.route('/lab2/calc/<int:a>/<int:b>')
+def calculate(a, b):
+    return f'''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Калькулятор</title>
+        </head>
+        <body>
+            <h1>Расчёт с параметрами:</h1>
+            <p>{a} + {b} = {a + b}</p>
+            <p>{a} - {b} = {a - b}</p>
+            <p>{a} * {b} = {a * b}</p>
+            <p>{a} / {b} = {(a / b) if b != 0 else 'Деление на ноль'}</p>
+            <p>{a}<sup>{b}</sup> = {a ** b}</p>
+        </body>
+    </html>
+    '''
+
+@app.route('/lab2/calc/')
+def calc():
+    return redirect('/lab2/calc/1/1')
+
+@app.route('/lab2/calc/<int:a>/')
+def calc_one(a):
+    return redirect(f'/lab2/calc/{a}/1')
+
+books = [
+    {"author": "Лев Толстой", "title": "Война и мир", "genre": "Роман", "pages": 1225},
+    {"author": "Фёдор Достоевский", "title": "Преступление и наказание", "genre": "Роман", "pages": 671},
+    {"author": "Михаил Булгаков", "title": "Мастер и Маргарита", "genre": "Фантастика", "pages": 470},
+    {"author": "Лев Толстой", "title": "Анна Каренина", "genre": "Роман", "pages": 864},
+    {"author": "Борис Пастернак", "title": "Доктор Живаго", "genre": "Роман", "pages": 704},
+    {"author": "Александр Пушкин", "title": "Евгений Онегин", "genre": "Поэма", "pages": 240},
+    {"author": "Фёдор Достоевский", "title": "Идиот", "genre": "Роман", "pages": 640},
+    {"author": "Иван Гончаров", "title": "Обломов", "genre": "Роман", "pages": 560},
+    {"author": "Николай Гоголь", "title": "Мёртвые души", "genre": "Роман", "pages": 352},
+    {"author": "Антон Чехов", "title": "Чайка", "genre": "Пьеса", "pages": 120}
+]
+
+
+@app.route('/lab2/books')
+def show_books():
+    return render_template('books.html', books=books)
+    
