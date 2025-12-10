583ca34dcadb4a46a1eca7a0ce469f984854910c
yas1k <yanson-andrei@mail.ru>
2025-12-10 13:14:42 +0700

    Форма + корректировка пола

--- lab3.py
+++ lab3.py
@@ -22,3 +22,17 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
+@lab3.route('/lab3/form1')
+def form1():
+    errors = {}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
