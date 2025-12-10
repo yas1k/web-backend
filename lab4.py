1394c44c6268cf9cc0c38c902a15f8856efb4b00
yas1k <yanson-andrei@mail.ru>
2025-12-10 14:24:18 +0700

    POST/Redirect/GET + Валидация

--- lab4.py
+++ lab4.py
@@ -112,3 +112,20 @@ def pow():
     result = x1 ** x2
 
     return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
+
+tree_count = 0
+
+@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
+
+    if operation == 'cut' and tree_count > 0:
+        tree_count -= 1
+    elif operation == 'plant' and tree_count < 10:
+        tree_count += 1
+
+    return redirect('/lab4/tree')
