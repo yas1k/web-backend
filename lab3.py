1710973f8ae9e1cefe7938c6dfe0072b759f14b1
yas1k <yanson-andrei@mail.ru>
2025-12-10 13:29:04 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -37,3 +37,27 @@ def form1():
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
 
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит 120 рублей, чёрный чай — 80 рублей, зелёный — 70 рублей.
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    # Добавка молока удорожает напиток на 30 рублей, а сахара — на 10.
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    
+    return render_template('lab3/pay.html', price=price)
