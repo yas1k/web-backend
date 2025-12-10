1070e52e620d51c4a0f7bb071b6ed8d9e4364086
yas1k <yanson-andrei@mail.ru>
2025-12-10 14:15:08 +0700

    Деление

--- lab4.py
+++ lab4.py
@@ -5,3 +5,26 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def laba4():
     return render_template('/lab4/lab4.html')
+
+
+@lab4.route('/lab4/div-form')
+def div_form():
+    return render_template('lab4/div-form.html')
+
+
+@lab4.route('/lab4/div', methods=['POST'])  
+def div():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if not x1 or not x2:
+        return render_template('lab4/div.html', error='Оба поля должны быть заполнены!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+    if x2 == 0:
+        return render_template('lab4/div.html', error='На ноль делить нельзя!')
+        
+    result = x1 / x2
+
+    return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
