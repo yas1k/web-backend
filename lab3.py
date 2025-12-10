c807f7a5efc27cbae4ec1df4eaa7717e994d55f6
yas1k <yanson-andrei@mail.ru>
2025-12-10 13:11:51 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, make_response, redirect
 
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
     return render_template('lab3/lab3.html')
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'magenta')
+    return resp
+
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
