4d138caa280d6d744019c692fc123a6d72bdac41
yas1k <yanson-andrei@mail.ru>
2026-03-04 02:32:39 +0700

    Коммит app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, url_for, redirect, request, abort, render_template
 import datetime
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from db.models import users
+from flask_login import LoginManager
+from datetime import timedelta
 
 from lab1 import lab1
 from lab2 import lab2
@@ -9,13 +15,35 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(login_id):
+    return users.query.get(int(login_id))
 
 app.config['SECRET_KEY'] = os.environ.get('SECRT_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
-#app.secret_key = 'секретно-секретный секрет'
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'andrey_yanson_orm'
+    db_user = 'andrey_yanson_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "andrey_yanson_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 
 app.register_blueprint(lab1)
@@ -25,6 +53,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 @app.route("/")
 def index2():
@@ -48,6 +77,7 @@ def index2():
                 <a href="/lab5/">Пятая лабораторная работа</a>
                 <a href="/lab6/">Шестая лабораторная работа</a>
                 <a href="/lab7/">Седьмая лабораторная работа</a>
+                <a href="/lab8/">Восьмая лабораторная работа</a>
             </main>
             <footer>
                 <hr>
